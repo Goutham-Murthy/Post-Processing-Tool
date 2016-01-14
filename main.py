5d958272b5c0b11f04c0f0c132e3f788edbe4b08
Goutham Murthy <goutham.murthy@outlook.com>
2016-01-15 00:34:47 +0100

    Main method for the Pre-Processing tool

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ heat_profiles,building_ids = LoadData.getHeatProfiles("D:/aja-gmu/Simulation_Fil
 for i in range(0,len(building_ids)):
     building_id = building_ids[i]
     thermal_profile = heat_profiles[i]
-    building_number = GenerateCases.PostProcessingTool(building_id=building_id,thermal_profile=thermal_profile)
+    building_number = GenerateCases.PreProcessingTool(building_id=building_id,thermal_profile=thermal_profile)
     building_number.generateCases()
     
 os.chdir("D:/aja-gmu/Simulation_Files")
