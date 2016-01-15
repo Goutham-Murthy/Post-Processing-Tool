4610d0c8feb93d47af26450b9d10a74a6e2e73b6
Goutham-Murthy <goutham2612@gmail.com>
2016-01-15 16:48:12 +0100

    Modifications

--- boiler.py
+++ boiler.py
@@ -4,136 +4,136 @@ Module consists class representing boilers.
 """
 import annuity
 
-class Boiler:
+
+class Boiler(annuity.Annuity):
     """Class representing boiler technology.
-    
+
     Attributes:
-    model (string): Model of the boiler.
-    thermal_capacity (float): Thermal capacity of the boiler [kW].
-    efficiency (float): Efficiency of the boiler [decimal<1].
-    heat_yearly (float): Sum value of heat provided by the boiler unit over the year[kWh].
-    heat_hourly (float): Hourly values of the heat provided by the boiler unit [kWh].
-    annuity (float) : Annuity of the boiler [Euros]
+        model (string)          : Model of the boiler.
+        thermal_capacity (float): Thermal capacity of the boiler [kW].
+        efficiency (float)      : Efficiency of the boiler [decimal<1].
+        heat_yearly (float)     : Sum value of heat provided by the boiler unit
+                                 over the year [kWh].
+        heat_hourly (float)     : Hourly values of the heat provided by the
+                                 boiler unit [kWh].
+        annuity (float)         : Annuity of the boiler [Euros].
+        emissions (float)       : CO2 emissions of the boiler [kg of CO2].
+        deperiod(float)         : Depreciation period [years].
+        finst(float)            : Effort for annual repairs as percentage of
+                                 initial investment [%].
+        fwins(float)            : Effort for annual maintenance and inspection
+                                 as percentage of total investment [%].
+        effop(float)            : Effort for operation [hours/annum].
     """
 
     def __init__(self, model, thermal_capacity, efficiency):
         """Constructor method for class Boiler.
-        
+
         Args:
-        model (string): Model of the boiler.
-        thermal_capacity (float): Thermal capacity of the boiler [kW].
-        efficiency (float): Efficiency of the boiler [decimal<1].
+            model (string): Model of the boiler.
+            thermal_capacity (float): Thermal capacity of the boiler [kW].
+            efficiency (float): Efficiency of the boiler [decimal<1].
         """
         self.model = model
         self.thermal_capacity = thermal_capacity
         self.efficiency = efficiency
-        # Initialising other attributes to zero.
+        # Initialising other variables to zero.
         self.heat_hourly = [0]*8760
         self.heat_yearly = 0
         self.annuity = 0
-        
-    def get_heat(self,required_heat,hour):
+        self.emissions = 0
+        # The deprecition period, finst, fwins and effop are different for
+        # different capacitites according to VDI 2067.
+        if self.thermal_capacity < 100:
+            self.deperiod = 18
+            self.finst = 1.5
+            self.fwins = 1.5
+            self.effop = 10
+        elif self.thermal_capacity in range(100, 200):
+            self.deperiod = 20
+            self.finst = 1.0
+            self.fwins = 1.5
+            self.effop = 20
+        else:
+            self.deperiod = 20
+            self.finst = 1.0
+            self.fwins = 2.0
+            self.effop = 20
+
+    def get_heat(self, required_heat, hour):
         """
-        Given the required heat, function calculates the hourly heat given by 
+        Given the required heat, function calculates the hourly heat met by
         the boiler and returns the value for unsatified thermal demand.
-        
+
         Args:
-        required_heat (float): Hourly heat demand of the building [kWh].
-        hour (int): Hour of the year.
-        
+            required_heat (float): Hourly heat demand of the building [kWh].
+            hour (int): Hour of the year.
+
         Returns:
-        required_heat (float): Hourly thermal demand not met by the boiler [kWh].
+            required_heat (float): Hourly thermal demand not met by the boiler
+                                 [kWh].
         """
-        # If thermal capacity is more than hourly thermal demand, meet the demand
-        # entirely.
+        # If thermal capacity is more than hourly thermal demand, meet the
+        # demand entirely.
         if required_heat <= self.thermal_capacity:
             self.heat_yearly += required_heat
             self.heat_hourly[hour] = required_heat
             required_heat = 0
-            return required_heat
-        # If hourly thermal demand is grreater than the capacity, meet as much as
-        # possible.
+        # If hourly thermal demand is grreater than the capacity, meet as much
+        # as possible.
         else:
             self.heat_yearly += self.thermal_capacity
             self.heat_hourly[hour] = self.thermal_capacity
             required_heat -= self.thermal_capacity
-            return required_heat
-        
-    def get_emissions(self):
+        return required_heat
+
+    def set_emissions(self):
         """
         Calculates the CO2 emissions of the boiler unit.
-        
+
         Args:
-        None
-        
+            None.
+
         Returns:
-        emissions (float): CO2 emissions of the boiler unit [kg of CO2].
+            None
         """
         # CO2 emissions of condensing boilers are about 56 g/MJ or 201.6 g/kWh.
-        # [R Dones, Thomas Heck, and S Hirschberg. Greenhouse gas emissions from energy systems:
-        # comparison and overview. 2004.]
-        emissions = (201.6*self.heat_yearly/1000)
-        return emissions
-        
-    def get_annuity(self):
-        """
-        Calculates the annuity of the boiler according to equations in VDI 2067.
-        
-        Args:
-        None
-        
-        Returns:
-        annuity (float): Annuity of the boiler unit [Euros]
-        """
-        obperiod = 10 #VDI2067
-        # The following factors are assumed. For boilers and other technologies
-        q = 1.07
-        r = 1.03
-        b = annuity.getb(q=q,r=r,obperiod=obperiod)
-        bv = b
-        bb = annuity.getb(q=q,r=1.02,obperiod=obperiod)
-        bi = b
-        # The deprecition period, finst,fwins and effop are different for different 
-        # capacitites according to VDI 2067    
-        if self.thermal_capacity < 100:
-            deperiod = 18
-            finst = 0.015
-            fwins = 0.015
-            effop = 10
-        elif self.thermal_capacity in range(100,200):
-            deperiod = 20
-            finst = 0.01
-            fwins = 0.015
-            effop = 20
-        else:
-            deperiod = 20
-            finst = 0.01
-            fwins = 0.02
-            effop = 20
-        
-        # Capital Related Costs
-        a = annuity.get_annuity_factor(q=q,obperiod=obperiod)
-        # Capital related costs for the boiler include price of purchase and 
-        # installation costs.
-        CRC = 79.061*self.thermal_capacity + 1229.8
-        Ank = annuity.get_Ank(A0=CRC,r=r,q=q,obperiod=obperiod,deperiod=deperiod)
-    
-        # Demand related costs include price of fuel to produce the required heat
-        # Price of gas is 0.067 Euros/kWh. [Eurostat. nrg_pc_205. Accessed: 2015-02-11.]
-        DRC = 0.067*self.heat/self.efficiency
-        Anv = DRC*a*bv
-        
-        #Operation related costs include maintanance and repair
-        ORC = 30*effop
-        Ain = CRC*(finst+fwins)
-        Anb = ORC*a*bb + Ain*a*bi
-    
-        #Other costs
-        Ans = 0
-        
-        #Proceeds
-        Ane = 0
-              
-        self.annuity = Ane - (Ank+Anv+Anb+Ans)        
-        return CRC,Ank,Anv,Anb,self.annuity 
+        # [R Dones, Thomas Heck, and S Hirschberg. Greenhouse gas emissions
+        # from energy systems:comparison and overview. 2004.]
+        self.emissions = (201.6*self.heat_yearly/1000)
+        return
 
+    def set_annuity(self):
+        """
+        Calculates the annuity of the boiler.
+
+        Args:
+            None.
+
+        Returns:
+            annuity (float): Annuity of the boiler unit [Euros].
+        """
+        # Capital related costs for the boiler include price of purchase and
+        # installation costs.
+        self.A0 = 79.061*self.thermal_capacity + 1229.8
+        self.set_Ank()
+
+        # Demand related costs include price of fuel to produce required heat
+        DRC = self.gas_price*self.heat/self.efficiency
+        self.Anv = DRC*self.a*self.bv
+
+        # Operation related costs include maintanance and repair
+        ORC = 30*self.effop
+        Ain = self.A0*(self.finst + self.fwins)/100   # finst and fwins in %
+        self.Anb = ORC*self.a*self.bb + Ain*self.a*self.bi
+
+        # Other costs
+        self.Ans = 0
+
+        # Proceeds
+        self.Ane = 0
+
+        self.annuity = self.Ane - (self.Ank + self.Anv + self.Anb + self.Ans)
+        return
+# b=Boiler('Model',11.2,0.98)
+# print b.emissions
