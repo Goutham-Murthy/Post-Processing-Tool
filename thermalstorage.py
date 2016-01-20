e0c9a3543386c927204aa699fadf3088182e347a
Goutham-Murthy <goutham2612@gmail.com>
2016-01-20 10:46:42 +0100

    Added annuity and other required methods

---
+++ thermalstorage.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import annuity
+
+
+class ThermalStorage(annuity.Annuity):
+    """
+    Class representing Thermal Storage technologgy.
+
+    Attributes:
+        model (string)          : Model of the thermal storage unit.
+        th_capacity (float)     : Thermal capacity of the stortage unit [kWh].
+        loss_percent (float)    : Percentage of heat lost in the thermal
+                                 storage unit over each hour [decimal<1].
+        heat_stored (float)     : Heat present in the thermal storage unit
+                                 at the particular hour [kWh].
+        heat_hourly (float)     : Hourly values of the heat provided by the
+                                 thermal storage unit [kWh].
+        annuity (float)         : Annuity of the thermal storage unit [Euros].
+        losses (float)          : Total heat losses from the thermal storage
+                                 unit [kWh]
+        deperiod(float)         : Depreciation period [years].
+        finst(float)            : Effort for annual repairs as percentage of
+                                 initial investment [%].
+        fwins(float)            : Effort for annual maintenance and inspection
+                                 as percentage of total investment [%].
+        effop(float)            : Effort for operation [hours/annum].
+
+    Extends:
+        Annuity class
+    """
+
+    def __init__(self, model, th_capacity, loss_percent):
+        """Constructor method for class thermal storage.
+
+        Args:
+            model (string)          : Model of the thermal storage unit.
+            th_capacity (float)     : Thermal capacity of the storage unit
+                                     [kWh].
+            loss_percent (float)    : Percentage of heat lost in the thermal
+                                     storage unit over each hour [decimal<1].
+        """
+        self.model = model
+        self.th_capacity = th_capacity
+        self.loss_percent = loss_percent
+        # Initialising other variables to zero.
+        self.heat_stored = [0]*8760
+        self.heat_hourly = [0]*8760
+        self.annuity = 0
+        self.losses = 0
+        self.deperiod = 15
+        self.finst = 2
+        self.fwins = 1
+        self.effop = 0
+        annuity.Annuity.__init__(self)
+
+    def get_heat(self, required_heat, hour):
+        """
+        Given the required heat, function calculates the hourly heat met by
+        the boiler and returns the value for unsatified thermal demand.
+
+        Args:
+            required_heat (float)   : Hourly heat demand of the building [kWh].
+            hour (int)              : Hour of the year.
+
+        Returns:
+            required_heat (float)   : Hourly thermal demand not met by the
+                                      boiler [kWh].
+        """
+        # If thermal capacity is more than hourly thermal demand, meet the
+        # demand entirely.
+        if required_heat <= self.heat_stored[hour]:
+            self.heat_hourly[hour] = required_heat
+            required_heat = 0
+        # If hourly thermal demand is grreater than the capacity, meet as much
+        # as possible.
+        else:
+            self.heat_hourly[hour] = self.heat_stored[hour]
+            required_heat -= self.heat_stored[hour]
+        return required_heat
+
+    def set_annuity(self):
+        """
+        Calculates the annuity of the thermal storage unit.
+
+        Args:
+            None.
+
+        Returns:
+            None
+        """
+        th_capacity_l = self.get_ThSt_Cap_l()
+
+        if th_capacity_l > 1000:
+            bonus = 250*th_capacity_l/1000
+        else:
+            bonus = 0
+        # Capital related costs for the boiler include price of purchase and
+        # installation costs.
+        self.A0 = 1.0912*th_capacity_l + 367.92 - bonus
+        self.set_Ank()
+
+        # Demand related costs include price of fuel to produce required heat
+        DRC = 0
+        self.Anv = DRC*self.a*self.bv
+
+        # Operation related costs include maintanance and repair
+        ORC = 30*self.effop
+        Ain = self.A0*(self.finst + self.fwins)/100   # finst and fwins in %
+        self.Anb = ORC*self.a*self.bb + Ain*self.a*self.bi
+
+        # Other costs
+        self.Ans = 0
+
+        # Proceeds
+        self.Ane = 0
+
+        self.annuity = self.Ane - (self.Ank + self.Anv + self.Anb + self.Ans)
+        return
+
+    def get_ThSt_Cap_l(self):
+        th_capacity_l =  self.th_capacity*3600000/(4180*40)
+        return th_capacity_l
