ed17c498d27b66c89005c6476c935b8d41643541
Goutham Murthy <goutham.murthy@outlook.com>
2016-02-05 01:26:33 +0100

    Threading added

--- ahoi.py
+++ ahoi.py
@@ -8,19 +8,11 @@
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
-import loaddata
-import preprocessingtool
-import os
 
 
-class Ui_MainWindow(object):
-    def __init__(self):
-        self.th_technologies_user = []
-        self.el_technologies_user = []
+class UiMainWindow(object):
 
-
-    def setupUi(self, MainWindow):
-
+    def setup_gui(self, MainWindow):
         MainWindow.setObjectName("SGT!!!!!!!!!!!!!")
         MainWindow.resize(555, 389)
         MainWindow.setAcceptDrops(True)
@@ -38,34 +30,34 @@ class Ui_MainWindow(object):
         self.label1.setWordWrap(True)
         self.label1.setObjectName("label1")
         self.verticalLayout.addWidget(self.label1)
-        self.CHP1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.CHP1.setObjectName("CHP1")
-        self.verticalLayout.addWidget(self.CHP1)
-        self.boiler1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.boiler1.setObjectName("boiler1")
-        self.verticalLayout.addWidget(self.boiler1)
-        self.thermalstorage1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.thermalstorage1.setObjectName("thermalstorage")
-        self.verticalLayout.addWidget(self.thermalstorage1)
-        self.solarthermal1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.solarthermal1.setObjectName("solarthermal")
-        self.verticalLayout.addWidget(self.solarthermal1)
-        self.electricheater1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.electricheater1.setObjectName("electricheater1")
-        self.verticalLayout.addWidget(self.electricheater1)
-        self.photovoltaics1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.photovoltaics1.setObjectName("photovoltaics1")
-        self.verticalLayout.addWidget(self.photovoltaics1)
-        self.electricstorage1 = QtGui.QCheckBox(self.verticalLayoutWidget)
-        self.electricstorage1.setObjectName("electricstorage1")
-        self.verticalLayout.addWidget(self.electricstorage1)
+        self.check_box_chp = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_chp.setObjectName("check_box_chp")
+        self.verticalLayout.addWidget(self.check_box_chp)
+        self.check_box_boiler = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_boiler.setObjectName("check_box_boiler")
+        self.verticalLayout.addWidget(self.check_box_boiler)
+        self.check_box_thst = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_thst.setObjectName("thermalstorage")
+        self.verticalLayout.addWidget(self.check_box_thst)
+        self.check_box_solth = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_solth.setObjectName("solarthermal")
+        self.verticalLayout.addWidget(self.check_box_solth)
+        self.check_box_elhe = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_elhe.setObjectName("check_box_elhe")
+        self.verticalLayout.addWidget(self.check_box_elhe)
+        self.check_box_pv = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_pv.setObjectName("check_box_pv")
+        self.verticalLayout.addWidget(self.check_box_pv)
+        self.check_box_elst = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.check_box_elst.setObjectName("check_box_elst")
+        self.verticalLayout.addWidget(self.check_box_elst)
         self.buttonBox = QtGui.QDialogButtonBox(self.centralwidget)
         self.buttonBox.setGeometry(QtCore.QRect(200, 250, 159, 31))
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Ok|QtGui.QDialogButtonBox.Cancel)
         self.buttonBox.setObjectName("buttonBox")
-        self.radioButton = QtGui.QRadioButton(self.centralwidget)
-        self.radioButton.setGeometry(QtCore.QRect(180, 220, 171, 16))
-        self.radioButton.setObjectName("radioButton")
+        self.rbtn_hourly_excels = QtGui.QRadioButton(self.centralwidget)
+        self.rbtn_hourly_excels.setGeometry(QtCore.QRect(180, 220, 171, 16))
+        self.rbtn_hourly_excels.setObjectName("rbtn_hourly_excels")
         self.layoutWidget = QtGui.QWidget(self.centralwidget)
         self.layoutWidget.setGeometry(QtCore.QRect(427, 30, 77, 154))
         self.layoutWidget.setObjectName("layoutWidget")
@@ -73,37 +65,37 @@ class Ui_MainWindow(object):
         self.verticalLayout_3.setSpacing(20)
         self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.btnOutputBrowse = QtGui.QPushButton(self.layoutWidget)
-        self.btnOutputBrowse.setObjectName("btnOutputBrowse")
-        self.verticalLayout_3.addWidget(self.btnOutputBrowse)
-        self.btnWeathreBrowse = QtGui.QPushButton(self.layoutWidget)
-        self.btnWeathreBrowse.setObjectName("btnWeathreBrowse")
-        self.verticalLayout_3.addWidget(self.btnWeathreBrowse)
-        self.btnHeatprofilesBrowse = QtGui.QPushButton(self.layoutWidget)
-        self.btnHeatprofilesBrowse.setObjectName("btnHeatprofilesBrowse")
-        self.verticalLayout_3.addWidget(self.btnHeatprofilesBrowse)
-        self.btnElProfileBrowse = QtGui.QPushButton(self.layoutWidget)
-        self.btnElProfileBrowse.setObjectName("btnElProfileBrowse")
-        self.verticalLayout_3.addWidget(self.btnElProfileBrowse)
+        self.btn_output_folder = QtGui.QPushButton(self.layoutWidget)
+        self.btn_output_folder.setObjectName("btn_output_folder")
+        self.verticalLayout_3.addWidget(self.btn_output_folder)
+        self.btn_weather_file = QtGui.QPushButton(self.layoutWidget)
+        self.btn_weather_file.setObjectName("btn_weather_file")
+        self.verticalLayout_3.addWidget(self.btn_weather_file)
+        self.btn_heat_profiles = QtGui.QPushButton(self.layoutWidget)
+        self.btn_heat_profiles.setObjectName("btn_heat_profiles")
+        self.verticalLayout_3.addWidget(self.btn_heat_profiles)
+        self.btn_electrical_profiles = QtGui.QPushButton(self.layoutWidget)
+        self.btn_electrical_profiles.setObjectName("btn_electrical_profiles")
+        self.verticalLayout_3.addWidget(self.btn_electrical_profiles)
         self.widget = QtGui.QWidget(self.centralwidget)
         self.widget.setGeometry(QtCore.QRect(210, 30, 211, 151))
         self.widget.setObjectName("widget")
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.widget)
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.OutputFolderLineEdit = QtGui.QLineEdit(self.widget)
-        self.OutputFolderLineEdit.setCursorMoveStyle(QtCore.Qt.LogicalMoveStyle)
-        self.OutputFolderLineEdit.setObjectName("OutputFolderLineEdit")
-        self.verticalLayout_2.addWidget(self.OutputFolderLineEdit)
-        self.WeatherDataLineEdit = QtGui.QLineEdit(self.widget)
-        self.WeatherDataLineEdit.setObjectName("WeatherDataLineEdit")
-        self.verticalLayout_2.addWidget(self.WeatherDataLineEdit)
-        self.HProfileLineEdit = QtGui.QLineEdit(self.widget)
-        self.HProfileLineEdit.setObjectName("HProfileLineEdit")
-        self.verticalLayout_2.addWidget(self.HProfileLineEdit)
-        self.ElProfileLineEdit = QtGui.QLineEdit(self.widget)
-        self.ElProfileLineEdit.setObjectName("ElProfileLineEdit")
-        self.verticalLayout_2.addWidget(self.ElProfileLineEdit)
+        self.line_edit_output_folder = QtGui.QLineEdit(self.widget)
+        self.line_edit_output_folder.setCursorMoveStyle(QtCore.Qt.LogicalMoveStyle)
+        self.line_edit_output_folder.setObjectName("line_edit_output_folder")
+        self.verticalLayout_2.addWidget(self.line_edit_output_folder)
+        self.line_edit_weather = QtGui.QLineEdit(self.widget)
+        self.line_edit_weather.setObjectName("line_edit_weather")
+        self.verticalLayout_2.addWidget(self.line_edit_weather)
+        self.line_edit_heat_profiles = QtGui.QLineEdit(self.widget)
+        self.line_edit_heat_profiles.setObjectName("line_edit_heat_profiles")
+        self.verticalLayout_2.addWidget(self.line_edit_heat_profiles)
+        self.line_edit_electrical_profiles = QtGui.QLineEdit(self.widget)
+        self.line_edit_electrical_profiles.setObjectName("line_edit_electrical_profiles")
+        self.verticalLayout_2.addWidget(self.line_edit_electrical_profiles)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 555, 21))
@@ -114,127 +106,29 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        QtCore.QObject.connect(self.CHP1, QtCore.SIGNAL("clicked(bool)"), self.CHP1.setChecked)
-        QtCore.QObject.connect(self.boiler1, QtCore.SIGNAL("clicked(bool)"), self.boiler1.setChecked)
-        QtCore.QObject.connect(self.electricheater1, QtCore.SIGNAL("clicked(bool)"), self.electricheater1.setChecked)
-        QtCore.QObject.connect(self.electricstorage1, QtCore.SIGNAL("clicked(bool)"), self.electricstorage1.setChecked)
-        QtCore.QObject.connect(self.photovoltaics1, QtCore.SIGNAL("clicked(bool)"), self.photovoltaics1.setChecked)
-        QtCore.QObject.connect(self.solarthermal1, QtCore.SIGNAL("clicked(bool)"), self.solarthermal1.setChecked)
-        QtCore.QObject.connect(self.thermalstorage1, QtCore.SIGNAL("clicked(bool)"), self.thermalstorage1.setChecked)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), self.run_ppt)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), self.update)
-        QtCore.QObject.connect(self.btnOutputBrowse, QtCore.SIGNAL("clicked()"), self.output_folder)
-        QtCore.QObject.connect(self.btnWeathreBrowse, QtCore.SIGNAL("clicked()"), self.weather_file1)
-        QtCore.QObject.connect(self.btnHeatprofilesBrowse, QtCore.SIGNAL("clicked()"), self.heat_profiles_file)
-        QtCore.QObject.connect(self.btnElProfileBrowse, QtCore.SIGNAL("clicked()"), self.el_profiles_file)
-        QtCore.QObject.connect(self.radioButton, QtCore.SIGNAL("clicked(bool)"), self.radioButton.setChecked)
+        QtCore.QObject.connect(self.check_box_chp, QtCore.SIGNAL("clicked(bool)"), self.check_box_chp.setChecked)
+        QtCore.QObject.connect(self.check_box_boiler, QtCore.SIGNAL("clicked(bool)"), self.check_box_boiler.setChecked)
+        QtCore.QObject.connect(self.check_box_elhe, QtCore.SIGNAL("clicked(bool)"), self.check_box_elhe.setChecked)
+        QtCore.QObject.connect(self.check_box_elst, QtCore.SIGNAL("clicked(bool)"), self.check_box_elst.setChecked)
+        QtCore.QObject.connect(self.check_box_pv, QtCore.SIGNAL("clicked(bool)"), self.check_box_pv.setChecked)
+        QtCore.QObject.connect(self.check_box_solth, QtCore.SIGNAL("clicked(bool)"), self.check_box_solth.setChecked)
+        QtCore.QObject.connect(self.check_box_thst, QtCore.SIGNAL("clicked(bool)"), self.check_box_thst.setChecked)
+        QtCore.QObject.connect(self.rbtn_hourly_excels, QtCore.SIGNAL("clicked(bool)"), self.rbtn_hourly_excels.setChecked)
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Welcome to the SGT UI", None, QtGui.QApplication.UnicodeUTF8))
         self.label1.setText(QtGui.QApplication.translate("MainWindow", "Choose technologies that can be present in the system:", None, QtGui.QApplication.UnicodeUTF8))
-        self.CHP1.setText(QtGui.QApplication.translate("MainWindow", "CHP", None, QtGui.QApplication.UnicodeUTF8))
-        self.boiler1.setText(QtGui.QApplication.translate("MainWindow", "Boiler", None, QtGui.QApplication.UnicodeUTF8))
-        self.thermalstorage1.setText(QtGui.QApplication.translate("MainWindow", "Thermal Storage", None, QtGui.QApplication.UnicodeUTF8))
-        self.solarthermal1.setText(QtGui.QApplication.translate("MainWindow", "Solar Thermal", None, QtGui.QApplication.UnicodeUTF8))
-        self.electricheater1.setText(QtGui.QApplication.translate("MainWindow", "Electric Heater", None, QtGui.QApplication.UnicodeUTF8))
-        self.photovoltaics1.setText(QtGui.QApplication.translate("MainWindow", "Photovoltaics", None, QtGui.QApplication.UnicodeUTF8))
-        self.electricstorage1.setText(QtGui.QApplication.translate("MainWindow", "Electric Storage", None, QtGui.QApplication.UnicodeUTF8))
-        self.radioButton.setText(QtGui.QApplication.translate("MainWindow", "Hourly excels required?", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnOutputBrowse.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnWeathreBrowse.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnHeatprofilesBrowse.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnElProfileBrowse.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
-
-    def update(self):
-        self.CHP1.setChecked(False)
-        self.boiler1.setChecked(False)
-        self.thermalstorage1.setChecked(False)
-        self.solarthermal1.setChecked(False)
-        self.electricheater1.setChecked(False)
-        self.photovoltaics1.setChecked(False)
-        self.electricstorage1.setChecked(False)
-        self.OutputFolderLineEdit.clear()
-        self.WeatherDataLineEdit.clear()
-        self.ElProfileLineEdit.clear()
-        self.HProfileLineEdit.clear()
-        self.radioButton.setChecked(False)
-
-
-    def output_folder(self):
-        dialog = QtGui.QFileDialog()
-        dialog.setFileMode(QtGui.QFileDialog.Directory)
-        dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)
-        self.output_folder_name = dialog.getExistingDirectory(dialog, 'Choose Directory',os.path.curdir)
-        self.OutputFolderLineEdit.insert(self.output_folder_name)
-        return
-
-    def weather_file1(self):
-        dialog = QtGui.QFileDialog()
-        # self.weather_file_location = dialog.getExistingDirectory(dialog, 'Choose Directory', os.path.curdir)
-        self.weather_file_name, discard = dialog.getOpenFileName(caption="Choose TRY weather File",
-                                                                 filter=".csv files(*.csv)")
-        self.WeatherDataLineEdit.insert(self.weather_file_name)
-        return
-
-    def el_profiles_file(self):
-        dialog = QtGui.QFileDialog()
-        # self.weather_file_location = dialog.getExistingDirectory(dialog, 'Choose Directory', os.path.curdir)
-        self.electrical_profiles_name, discard = dialog.getOpenFileName(caption="Choose file with electrical profiles",
-                                                                        filter="Excel files(*.xlsx *.xls)",
-                                                                        )
-        self.ElProfileLineEdit.insert(self.electrical_profiles_name)
-        return
-
-    def heat_profiles_file(self):
-        dialog = QtGui.QFileDialog()
-        # self.weather_file_location = dialog.getExistingDirectory(dialog, 'Choose Directory', os.path.curdir)
-        self.heat_profiles_name, discard = dialog.getOpenFileName(caption="Choose file with heat profiles",
-                                                                   filter="Excel files(*.xlsx *.xls)",
-                                                                  )
-        self.HProfileLineEdit.insert(self.heat_profiles_name)
-        return
-
-    def run_ppt(self):
-        print self.weather_file_name
-        global_radiation = loaddata.get_weather_data(self.weather_file_name)
-        heat_profiles, building_ids = loaddata.get_heat_profiles(self.heat_profiles_name)
-        el_profiles, building_ids = loaddata.get_el_profiles(self.electrical_profiles_name)
-
-        if self.CHP1.isChecked():
-            self.th_technologies_user.append('CHP')
-        if self.boiler1.isChecked():
-            self.th_technologies_user.append('B')
-        if self.thermalstorage1.isChecked():
-            self.th_technologies_user.append('ThSt')
-        if self.solarthermal1.isChecked():
-            self.th_technologies_user.append('SolTh')
-        if self.electricheater1.isChecked():
-            self.th_technologies_user.append('ElHe')
-        if self.electricstorage1.isChecked():
-            self.el_technologies_user.append('ElSt')
-        if self.photovoltaics1.isChecked():
-            self.el_technologies_user.append('PV')
-        print self.th_technologies_user, self.el_technologies_user
-
-        if self.radioButton.isChecked():
-            hourly_excels = True
-        else:
-            hourly_excels = False
-
-        for i in range(0, len(building_ids)):
-            building_id = building_ids[i]
-            thermal_profile = heat_profiles[i]
-            electrical_profile = el_profiles[i]
-            building_number = preprocessingtool.PreProcessingTool(building_id=building_id,
-                                                                  thermal_profile=thermal_profile,
-                                                                  electrical_profile=electrical_profile,
-                                                                  global_radiation=global_radiation,
-                                                                  th_technologies=self.th_technologies_user,
-                                                                  el_technologies=self.el_technologies_user,
-                                                                  location=self.output_folder_name,
-                                                                  hourly_excels=hourly_excels)
-            building_number.generate_cases()
-
-        os.chdir("D:/aja-gmu/Simulation_Files")
+        self.check_box_chp.setText(QtGui.QApplication.translate("MainWindow", "CHP", None, QtGui.QApplication.UnicodeUTF8))
+        self.check_box_boiler.setText(QtGui.QApplication.translate("MainWindow", "Boiler", None, QtGui.QApplication.UnicodeUTF8))
+        self.check_box_thst.setText(QtGui.QApplication.translate("MainWindow", "Thermal Storage", None, QtGui.QApplication.UnicodeUTF8))
+        self.check_box_solth.setText(QtGui.QApplication.translate("MainWindow", "Solar Thermal", None, QtGui.QApplication.UnicodeUTF8))
+        self.check_box_elhe.setText(QtGui.QApplication.translate("MainWindow", "Electric Heater", None, QtGui.QApplication.UnicodeUTF8))
+        self.check_box_pv.setText(QtGui.QApplication.translate("MainWindow", "Photovoltaics", None, QtGui.QApplication.UnicodeUTF8))
+        self.check_box_elst.setText(QtGui.QApplication.translate("MainWindow", "Electric Storage", None, QtGui.QApplication.UnicodeUTF8))
+        self.rbtn_hourly_excels.setText(QtGui.QApplication.translate("MainWindow", "Hourly excels required?", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_output_folder.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_weather_file.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_heat_profiles.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_electrical_profiles.setText(QtGui.QApplication.translate("MainWindow", "Browse..", None, QtGui.QApplication.UnicodeUTF8))
